1126f618259864b5d008c2a4b851010b77899ecf
CjangCjengh <101577701+CjangCjengh@users.noreply.github.com>
2022-10-15 19:52:24 +0800

    添加项目文件。

---
+++ VBBData/DataConverter.cs
@@ -0,0 +1,26 @@
+﻿using CoreSystem.AdvSystem;
+using Ionic.Zlib;
+using MsgPack.Serialization;
+using System.IO;
+using System.Web.Script.Serialization;
+
+namespace VBBData
+{
+    internal static class DataConverter
+    {
+        public static string BytesToJson(string path)
+        {
+            ViewControlRoot result;
+            using (FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                using (DeflateStream deflateStream = new DeflateStream(fileStream, CompressionMode.Decompress))
+                {
+                    MessagePackSerializer<ViewControlRoot> messagePackSerializer = MessagePackSerializer.Get<ViewControlRoot>();
+                    result = messagePackSerializer.Unpack(deflateStream);
+                }
+            }
+
+            return new JavaScriptSerializer().Serialize(result);
+        }
+    }
+}
---
+++ VBBData/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace VBBData
+{
+    internal static class Program
+    {
+        [DllImport("kernel32.dll")]
+        public static extern bool AllocConsole();
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main(string[] argv)
+        {
+            AllocConsole();
+            foreach (string path in argv)
+            {
+                try
+                {
+                    string jsonData = DataConverter.BytesToJson(path);
+                    string jsonPath = Path.ChangeExtension(path, "json");
+                    using (StreamWriter sw = new StreamWriter(jsonPath))
+                        sw.WriteLine(jsonData);
+                    Console.WriteLine($"{path} => {jsonPath}");
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}
